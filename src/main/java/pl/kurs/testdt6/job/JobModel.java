7b46360ebc16599f1dc5b0e60a688051e1fe1af7
dawid <dawid.taczkowski@outlook.com>
2022-01-30 21:14:46 +0100

    Add All and No args constructors

--- src/main/java/pl/kurs/testdt6/job/JobModel.java
+++ src/main/java/pl/kurs/testdt6/job/JobModel.java
@@ -1,8 +1,12 @@
 package pl.kurs.testdt6.job;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JobModel {
     private String path;
 }
