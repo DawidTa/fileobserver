b222a594b7c80af6246c2e1ed5da0cf04d087da9
dawid <dawid.taczkowski@outlook.com>
2022-02-03 11:13:34 +0100

    Remove repository annotation

--- src/main/java/pl/kurs/testdt6/file/FileRepository.java
+++ src/main/java/pl/kurs/testdt6/file/FileRepository.java
@@ -1,9 +1,7 @@
 package pl.kurs.testdt6.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FileRepository extends JpaRepository<FileEntity, String> {
     FileEntity findByFilePath(String path);
 }
