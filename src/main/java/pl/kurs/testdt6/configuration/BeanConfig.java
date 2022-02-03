90cc9f7db37c90df6b46dc8bafe4811c162a8485
dawid <dawid.taczkowski@outlook.com>
2022-02-03 11:11:35 +0100

    Swagger configuration

--- src/main/java/pl/kurs/testdt6/configuration/BeanConfig.java
+++ src/main/java/pl/kurs/testdt6/configuration/BeanConfig.java
@@ -4,19 +4,58 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import pl.kurs.testdt6.exception.ExceptionModel;
-import pl.kurs.testdt6.file.FileModel;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableAsync
+@EnableSwagger2
 public class BeanConfig {
 
+
     @Bean
-    FileModel fileModel() {
-        return new FileModel();
+    public ExceptionModel exceptionModel() {
+        return new ExceptionModel();
     }
 
     @Bean
-    ExceptionModel exceptionModel() {
-        return new ExceptionModel();
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .paths(PathSelectors.regex("/follow.*"))
+                .apis(RequestHandlerSelectors.basePackage("pl.kurs.testdt6"))
+                .build()
+                .apiInfo(apiDetails())
+                .securitySchemes(sechemeList());
+    }
+
+    private List<? extends SecurityScheme> sechemeList() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new BasicAuth("basicAuth"));
+        return schemeList;
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "File watch API",
+                "API for watching file on server",
+                "1.0",
+                "Free",
+                new springfox.documentation.service.Contact("test contact", "contact url", "contact@email.com"),
+                "API License",
+                "License url",
+                Collections.emptyList()
+        );
     }
 }
